071a62a8218e4b5f29213f28c4a2cc3de0d69c02
ElianEstrada <chictibiris@gmail.com>
2021-08-27 22:32:19 -0600

    grammar finished

--- grammar.py
+++ grammar.py
@@ -43,6 +43,13 @@ def t_error(t):
 # import ply.lex as lex
 # lexer = lex.lex()
     
+#methods
+temp = 0
+def new_temp() -> str:
+    global temp
+    temp += 1
+    return f't{temp}'
+
 
 #Definition Grammar
 
@@ -52,18 +59,20 @@ start = 'S'
 def p_S(t):
     'S : E'
 
-    t[0] = print(t[1])
+    t[0] = t[1]
 
 ##-------------Production E-------------##
 def p_E_add(t):
     'E : E tk_add T'
 
-    t[0] = t[1]
+    temp =  new_temp()
+    t[0] = {'tmp': temp, 'c3d': f"{t[1]['c3d']}{t[3]['c3d']}{temp} = {t[1]['tmp']} + {t[3]['tmp']}\n"}
 
 def p_E_sub(t):
     'E : E tk_sub T'
 
-    t[0] = t[1]
+    temp =  new_temp()
+    t[0] = {'tmp': temp, 'c3d': f"{t[1]['c3d']}{t[3]['c3d']}{temp} = {t[1]['tmp']} - {t[3]['tmp']}\n"}
 
 def p_E_T(t):
     'E : T'
@@ -74,12 +83,14 @@ def p_E_T(t):
 def p_T_mult(t):
     'T : T tk_mult F'
 
-    t[0] = t[1]
+    temp =  new_temp()
+    t[0] = {'tmp': temp, 'c3d': f"{t[1]['c3d']}{t[3]['c3d']}{temp} = {t[1]['tmp']} * {t[3]['tmp']}\n"}
 
 def p_T_div(t):
     'T : T tk_div F'
 
-    t[0] = t[1]
+    temp =  new_temp()
+    t[0] = {'tmp': temp, 'c3d': f"{t[1]['c3d']}{t[3]['c3d']}{temp} = {t[1]['tmp']} / {t[3]['tmp']}\n"}
 
 def p_T_F(t):
     'T : F'
@@ -95,7 +106,7 @@ def p_F_agroup(t):
 def p_F_id(t):
     'F : tk_id'
 
-    t[0] = t[1]
+    t[0] = {'tmp': t[1], 'c3d': '' }
 
 
 import ply.lex as lex
@@ -104,5 +115,6 @@ import ply.yacc as yacc
 lexer = lex.lex()
 parser = yacc.yacc()
 
-print(parser.parse('var1 + var2'))
+print('Input:\nvar1 / var2 * var3\n')
+print(f"Out:\n{parser.parse('var1 / var2 * var3')['c3d']}")
 
